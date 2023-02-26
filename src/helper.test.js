42f2c3b80142168d051645fed4ab5a62b3b45a7b
Torben <59419684+entorb@users.noreply.github.com>
2023-02-26 10:53:12 +0100

    Update helper.test.js

--- src/helper.test.js
+++ src/helper.test.js
@@ -15,8 +15,15 @@ npm test
 // import remaining_seconds_to_readable_time from "./helper.js";
 // throws SyntaxError: Cannot use import statement outside a module
 // so using require instead
-const { rel_seconds_to_readable_time } = require("./helper");
 
+const { zeroPad } = require("./helper");
+describe("Testing zeroPad", () => {
+  test("1->01", () => {
+    expect(zeroPad(1, 2)).toEqual("01");
+  });
+});
+
+const { rel_seconds_to_readable_time } = require("./helper");
 describe("Testing remaining_seconds_to_readable_time()", () => {
   test("1s", () => {
     expect(rel_seconds_to_readable_time(1)).toEqual("1s");
@@ -35,5 +42,25 @@ describe("Testing remaining_seconds_to_readable_time()", () => {
   });
   test(">1d", () => {
     expect(rel_seconds_to_readable_time(100000)).toEqual("1d 4h");
+  });
+});
+
+const { calc_speed_in_unit } = require("./helper");
+describe("Testing calc_speed_in_unit()", () => {
+  test("Minute", () => {
+    expect(calc_speed_in_unit(1234.567, "Minute")).toEqual(1234.6);
+  });
+  test("Hour", () => {
+    expect(calc_speed_in_unit(0.1, "Hour")).toEqual(6.0);
+  });
+  test("Day", () => {
+    expect(calc_speed_in_unit(0.1, "Day")).toEqual(144.0);
+  });
+});
+
+const { timestamp_to_datestr } = require("./helper");
+describe("Testing calc_speed_in_unit()", () => {
+  test("Minute", () => {
+    expect(timestamp_to_datestr(1677320618262)).toEqual("25.2.2023 11:23:38");
   });
 });
