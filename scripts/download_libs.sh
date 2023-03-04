dc72f02c23af69918df5548f6f3b0177b8d963a3
Torben <59419684+entorb@users.noreply.github.com>
2023-03-04 08:56:32 +0100

    download_libs.sh use pinned tabulator version

--- scripts/download_libs.sh
+++ scripts/download_libs.sh
@@ -6,9 +6,28 @@ cd $script_dir/..
 
 mkdir -p src/lib
 
-wget https://raw.githubusercontent.com/apache/echarts/5.4.1/dist/echarts.min.js -O src/lib/echarts-5.4.1.min.js
+# ECharts
+ver=5.4.1
+wget -q https://raw.githubusercontent.com/apache/echarts/$ver/dist/echarts.min.js -O src/lib/echarts-$ver.min.js
 
-wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/js/tabulator.min.js -O src/lib/tabulator-5.4.min.js
-wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/js/tabulator.min.js.map -O src/lib/tabulator.min.js.map
-wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/css/tabulator.min.css -O src/lib/tabulator.min.css
-wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/css/tabulator.min.css.map -O src/lib/tabulator.min.css.map
+# Tabulator
+ver=5.4.4
+
+mkdir -p tmp-dl
+wget -q https://github.com/olifolkerd/tabulator/archive/refs/tags/$ver.zip -O tmp-dl/tabulator-$ver.zip
+
+cd tmp-dl
+unzip -q -o tabulator-$ver.zip
+cd ..
+
+mv tmp-dl/tabulator-$ver/dist/js/tabulator.min.js src/lib/tabulator-5.4.min.js
+mv tmp-dl/tabulator-$ver/dist/js/tabulator.min.js.map src/lib/tabulator.min.js.map
+mv tmp-dl/tabulator-$ver/dist/css/tabulator.min.css src/lib/tabulator.min.css
+mv tmp-dl/tabulator-$ver/dist/css/tabulator.min.css.map src/lib/tabulator.min.css.map
+
+rm -r tmp-dl
+
+# wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/js/tabulator.min.js -O src/lib/tabulator-5.4.min.js
+# wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/js/tabulator.min.js.map -O src/lib/tabulator.min.js.map
+# wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/css/tabulator.min.css -O src/lib/tabulator.min.css
+# wget https://raw.githubusercontent.com/olifolkerd/tabulator/master/dist/css/tabulator.min.css.map -O src/lib/tabulator.min.css.map
